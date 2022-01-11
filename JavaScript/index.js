6780a41e84bf5aa39a959563c2636595992c1f12
Vijay-Thumar <vijaythumar1020@gmail.com>
2022-01-11 14:08:30 +0530

    added jquary and init document ready method

---
+++ JavaScript/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+"use strict";
+
+alert("Hola amigos");
+console.log("hi amigos from console");
+
+var resultdiv = document.getElementById("gm");
+resultdiv.innerHTML = "Buenos dias!! for espional's";
+
+});  
